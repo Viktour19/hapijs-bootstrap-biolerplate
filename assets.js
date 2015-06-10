549366e7a75706d62039e1180f46287894e841a3
Victor <viktour19@gmail.com>
2015-06-10 19:21:31 +0100

    initial commit

---
+++ assets.js
@@ -0,0 +1,11 @@
+// assets to be used by the 'hapi-assets' module based on process.env.NODE_ENV
+module.exports = {
+    development: {
+        js: ['dist/js/jquery.js', 'dist/js/bootstrap.min.js', 'js/scripts.js', 'js/ie10-viewport-bug-workaround.js'],
+        css: ['dist/css/bootstrap.min.css', 'css/styles.css', 'css/carousel.css']
+    },
+    production: {
+             js: ['dist/js/jquery.js', 'dist/js/bootstrap.min.js', 'js/scripts.js', 'js/ie10-viewport-bug-workaround.js'],
+        css: ['dist/css/bootstrap.min.css', 'css/styles.css', 'css/carousel.css']
+    }
+}
---
+++ server/assets/index.js
@@ -0,0 +1,73 @@
+// These are the public assets. Goal is to serve css, js, partials, images, or bower packages.
+exports.register = function(server, options, next){
+
+    server.route([
+        {
+            method: 'GET',
+            path: '/partials/{path*}',
+            config: {
+                handler: {
+                    directory: { path: './server/views/partials' }
+                },
+                id: 'partials'
+            }
+        },
+        {
+            method: 'GET',
+            path: '/images/{path*}',
+            config: {
+                handler: {
+                    directory: { path: './public/images' }
+                },
+                id: 'images'
+            }
+        },
+        {
+            method: 'GET',
+            path: '/css/{path*}',
+            config: {
+                handler: {
+                    directory: { path: './public/css' }
+                },
+                id: 'css'
+            }
+        },
+        {
+            method: 'GET',
+            path: '/js/{path*}',
+            config: {
+                handler: {
+                    directory: { path: './public/js' }
+                },
+                id: 'js'
+            }
+        },
+        {
+            method: 'GET',
+            path: '/bower_components/{path*}',
+            config: {
+                handler: {
+                    directory: { path: './public/bower_components' }
+                },
+                id: 'bower'
+            }
+        }
+        ,
+        {
+            method: 'GET',
+            path: '/dist/{path*}',
+            config: {
+                handler: {
+                    directory: { path: './node_modules/bootstrap/dist' }
+                },
+                id: 'dist'
+            }
+        }
+    ]);
+    
+    next();
+}
+
+exports.register.attributes = {
+    name: 'assets'
+};
